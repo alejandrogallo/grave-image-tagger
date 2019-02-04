afddbd2a9d0191dcbf0533bff5b4367ee654acee
Alejandro Gallo <aamsgallo@gmail.com>
2019-02-04 11:52:32 +0100

    Update fabric based tagger

--- js/fabric.js
+++ js/fabric.js
@@ -15,87 +15,49 @@ console.log(canvas);
 console.log(imageWidth);
 console.log(imageHeight);
 
-//var stage = new Konva.Stage({
-  //container: 'container',
-  //width: imageWidth,
-  //height: imageHeight
-//});
-
-//var imageLayer = new Konva.Layer();
-//var textLayer = new Konva.Layer();
-
-//function addNewWord() {
-  //words.push({ text: "New Word", x: 0.5, y: 0.5 });
-  //console.log(words);
-  //addWord(words[words.length - 1]);
-  //stage.draw();
-//}
+function addNewWord() {
+  words.push({ text: "New Word", x: 0.5, y: 0.5 });
+  console.log(words);
+  addWord(words[words.length - 1]);
+}
 
 function addWord(word) {
-
   var text = new fabric.IText(word.text, {
     left: word.x * imageWidth,
     top: word.y * imageHeight,
     word: word,
-    strokeWidth: 2,
+    strokeWidth: 1,
     stroke: '#aa0000',
   });
-
   canvas.add(text);
-
 }
 
-//var img = new Image();
-//img.onload = function() {
+fabric.Image.fromURL('images/grave.jpeg', function(img) {
 
-  //var graveImage = new Konva.Image({
-    //x: 0,
-    //y: 0,
-    //image: img,
-    //width: imageWidth,
-    //height: imageHeight,
-  //});
+  canvas.add(img.set({
+    left: 0,
+    top: 0,
+    hasControls: false,
+    selectable: false,
+    lockMovementX: true,
+    lockMovementY: true,
+    backgroundColor: 'blue',
+    opacity: 0.4,
+    width: imageWidth,
+    height: imageHeight,
+  }).scale(1));
 
-  //imageLayer.add(graveImage);
-  //imageLayer.draw();
+  img.on('select', function(evt) {
+    console.log('Moving');
+    alert(evt.target);
+  });
 
-//};
-//img.src = 'images/grave.jpeg';
+  words.forEach(function(word, index){
+    addWord(word);
+  });
 
-words.forEach(function(word, index){
-  addWord(word);
 });
 
 canvas.requestRenderAll();
 canvas.renderAll();
 
-//stage.add(imageLayer, textLayer);
-
-//textLayer.on('dragstart', function(evt) {
-  //var shape = evt.target;
-  //stage.draw();
-//});
-
-//textLayer.on('dblclick', function(evt) {
-  //var shape = evt.target;
-  //console.log(shape.attrs.x/imageWidth);
-  //console.log(shape.attrs.y/imageHeight);
-  //console.log(words[0]);
-//});
-
-//stage.on('dragend', function(evt) {
-  //var shape = evt.target;
-  //stage.draw();
-  //// set the word attributes also
-  //shape.attrs.word.x = shape.attrs.x / imageWidth;
-  //shape.attrs.word.y = shape.attrs.y / imageHeight;
-//});
-
-//stage.on('wheel', function(evt) {
-  //console.log("wheeling");
-  ////console.log(evt.deltaY);
-  //console.log(evt);
-  //var dScale = evt.ev.deltaY > 0 ? 0.1 : -0.1;
-  //stage.scale({x: stage.scaleX() + dScale, y:stage.scaleY() + dScale});
-  //stage.draw();
-//});
